26da4d7da709f1504a36a3da83d75b0502171966
Hana <37437842+lecheln@users.noreply.github.com>
2019-02-18 16:12:51 +0900

    dp

--- 11660 구간 합 구하기 5.c
+++ 11660 구간 합 구하기 5.c
@@ -1,10 +1,10 @@
-int dp[1025][1025],arr[1025][1025],n,m;
+int dp[1025][1025],n,m;
 int main(){
     scanf("%d%d",&n,&m);
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            scanf("%d",&arr[i][j]);
-            dp[i][j]=arr[i][j]+dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1];
+            scanf("%d",&dp[i][j]);
+            dp[i][j] += dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1];
         }
     }
     for(int i=0;i<m;i++){
